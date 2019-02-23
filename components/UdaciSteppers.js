0d8b787c5e196a6ce98e56be77d4b318341ee4e5
Bruno Carpinelli <brunogcpinheiro@hotmail.com>
2019-02-23 15:44:11 -0300

    add AddEntry Component, DateHeader, TextButton, UdaciSlider and UdaciSteppers

---
+++ components/UdaciSteppers.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
+
+export default function UdaciSteppers ({
+	max,
+	step,
+	unit,
+	value,
+	onIncrement,
+	onDecrement,
+}) {
+	return (
+		<View>
+			<View>
+				<TouchableOpacity onPress={onDecrement}>
+					<FontAwesome name="minus" color="black" size={30} />
+				</TouchableOpacity>
+				<TouchableOpacity onPress={onIncrement}>
+					<FontAwesome name="plus" color="black" size={30} />
+				</TouchableOpacity>
+			</View>
+			<View>
+				<Text>{value}</Text>
+				<Text>{unit}</Text>
+			</View>
+		</View>
+	);
+}
