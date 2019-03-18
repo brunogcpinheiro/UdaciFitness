2abbe0d4d199b07e77b5922bc6240dc6f8811337
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-03-18 11:57:48 -0300

    update data and reset data

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,12 @@
 // utils/helpers.js
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 import {
 	FontAwesome,
 	MaterialIcons,
 	MaterialCommunityIcons,
-} from '@expo/vector-icons';
-import { white } from './colors';
+} from "@expo/vector-icons";
+import { white } from "./colors";
 
 export function isBetween (num, x, y) {
 	if (num >= x && num <= y) {
@@ -17,28 +17,28 @@ export function isBetween (num, x, y) {
 }
 
 export function calculateDirection (heading) {
-	let direction = '';
+	let direction = "";
 
 	if (isBetween(heading, 0, 22.5)) {
-		direction = 'Norte';
+		direction = "Norte";
 	} else if (isBetween(heading, 22.5, 67.5)) {
-		direction = 'Nordeste';
+		direction = "Nordeste";
 	} else if (isBetween(heading, 67.5, 112.5)) {
-		direction = 'Leste';
+		direction = "Leste";
 	} else if (isBetween(heading, 112.5, 157.5)) {
-		direction = 'Sudeste';
+		direction = "Sudeste";
 	} else if (isBetween(heading, 157.5, 202.5)) {
-		direction = 'Sul';
+		direction = "Sul";
 	} else if (isBetween(heading, 202.5, 247.5)) {
-		direction = 'Suldoeste';
+		direction = "Suldoeste";
 	} else if (isBetween(heading, 247.5, 292.5)) {
-		direction = 'Oeste';
+		direction = "Oeste";
 	} else if (isBetween(heading, 292.5, 337.5)) {
-		direction = 'Noroeste';
+		direction = "Noroeste";
 	} else if (isBetween(heading, 337.5, 360)) {
-		direction = 'Norte';
+		direction = "Norte";
 	} else {
-		direction = 'Calculando...';
+		direction = "Calculando...";
 	}
 
 	return direction;
@@ -49,82 +49,88 @@ export function timeToString (time = Date.now()) {
 	const todayUTC = new Date(
 		Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()),
 	);
-	return todayUTC.toISOString().split('T')[0];
+	return todayUTC.toISOString().split("T")[0];
 }
 
 export function getMetricMetaInfo (metric) {
 	const info = {
 		run: {
-			displayName: 'Run',
+			displayName: "Run",
 			max: 50,
-			unit: 'Kilometers',
+			unit: "Kilometers",
 			step: 1,
-			type: 'steppers',
+			type: "steppers",
 			getIcon () {
 				return (
 					<View>
-						<MaterialIcons name="directions-run" color={'black'} size={35} />
+						<MaterialIcons name="directions-run" color={"black"} size={35} />
 					</View>
 				);
 			},
 		},
 		bike: {
-			displayName: 'Bike',
+			displayName: "Bike",
 			max: 100,
-			unit: 'kilometers',
+			unit: "kilometers",
 			step: 1,
-			type: 'steppers',
+			type: "steppers",
 			getIcon () {
 				return (
 					<View>
-						<MaterialCommunityIcons name="bike" color={'black'} size={35} />
+						<MaterialCommunityIcons name="bike" color={"black"} size={35} />
 					</View>
 				);
 			},
 		},
 		swim: {
-			displayName: 'Swim',
+			displayName: "Swim",
 			max: 9900,
-			unit: 'meters',
+			unit: "meters",
 			step: 100,
-			type: 'steppers',
+			type: "steppers",
 			getIcon () {
 				return (
 					<View>
-						<MaterialCommunityIcons name="swim" color={'black'} size={35} />
+						<MaterialCommunityIcons name="swim" color={"black"} size={35} />
 					</View>
 				);
 			},
 		},
 		sleep: {
-			displayName: 'Sleep',
+			displayName: "Sleep",
 			max: 24,
-			unit: 'hours',
+			unit: "hours",
 			step: 1,
-			type: 'slider',
+			type: "slider",
 			getIcon () {
 				return (
 					<View>
-						<FontAwesome name="bed" color={'black'} size={35} />
+						<FontAwesome name="bed" color={"black"} size={35} />
 					</View>
 				);
 			},
 		},
 		eat: {
-			displayName: 'Eat',
+			displayName: "Eat",
 			max: 10,
-			unit: 'rating',
+			unit: "rating",
 			step: 1,
-			type: 'slider',
+			type: "slider",
 			getIcon () {
 				return (
 					<View>
-						<MaterialCommunityIcons name="food" color={'black'} size={35} />
+						<MaterialCommunityIcons name="food" color={"black"} size={35} />
 					</View>
 				);
 			},
 		},
 	};
 
-	return typeof metric === 'undefined' ? info : info[metric];
+	export function getDailyReminderValue () {
+		return {
+			today: "Don't forget to log your data today!",
+		};
+	}
+
+	return typeof metric === "undefined" ? info : info[metric];
 }
